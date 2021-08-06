be5f1c9b357f6e56ff81cb7ccc437a7b94541771
Pablo Ronaldo Guevara <Ronaldo@DESKTOP-919BAJ2>
2021-08-06 13:25:06 -0600

    #1->Inicio de laboratorio

---
+++ src/lab3p2_pabloguevara/Lab3P2_PabloGuevara.java
@@ -0,0 +1,9 @@
+package lab3p2_pabloguevara;
+
+public class Lab3P2_PabloGuevara {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/lab3p2_pabloguevara/entidades.java
@@ -0,0 +1,62 @@
+package lab3p2_pabloguevara;
+
+public class entidades {
+    private String nombre;
+    private String apellido;
+    private String ID;
+    private String usuario;
+    private String contraseña;
+
+    public entidades() {
+    }
+
+    public entidades(String nombre, String apellido, String ID, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ID = ID;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+}
